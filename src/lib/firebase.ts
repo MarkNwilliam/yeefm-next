fdf8f9f2ed64fba0014045a679485a9d235e4283
Nkugwa Mark William <nkugwamarkwilliam.com>
2025-09-05 18:17:55 +0300

    news 4

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, Auth, onAuthStateChanged, User } from "firebase/auth";
 import { getAnalytics, Analytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: 'AIzaSyBXS56tIro9cd-Sd4ySn5AwLw3T6cnMHr0',
+  authDomain: 'yeeplatform.firebaseapp.com',
+  projectId: 'yeeplatform',
+  storageBucket: 'yeeplatform.appspot.com',
+  messagingSenderId: '272587831821',
+  appId: '1:272587831821:web:b612b427ff43968f33f344',
+  measurementId: 'G-YZV03469PP'
 };
 
 // Validate Firebase configuration
